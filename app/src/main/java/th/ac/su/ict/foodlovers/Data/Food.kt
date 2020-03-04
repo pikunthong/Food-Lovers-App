c975ae770adac0391e0b3c52058c9b561618a4c4
pikun <57031274+pikunthong@users.noreply.github.com>
2020-03-05 02:21:05 +0700

    update3

---
+++ app/src/main/java/th/ac/su/ict/foodlovers/Data/Food.kt
@@ -0,0 +1,10 @@
+package th.ac.su.ict.foodlovers.Data
+
+data class Food(
+    val imageFile:String,
+    val restaurantName:String,
+    val foodName:String,
+    val description:String,
+    val price:String,
+    val star:Int
+)
--- app/src/main/java/th/ac/su/ict/foodlovers/MainActivity.kt
+++ app/src/main/java/th/ac/su/ict/foodlovers/MainActivity.kt
@@ -2,11 +2,39 @@ package th.ac.su.ict.foodlovers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
+import th.ac.su.ict.foodlovers.Data.Food
+import th.ac.su.ict.foodlovers.Data.FoodAdapter
+import th.ac.su.ict.foodlovers.Utils.getJsonDataFromAsset
 
 class MainActivity : AppCompatActivity() {
+
+    var itemList:ArrayList<Food> = ArrayList<Food>()
+    lateinit var arrayAdapter: ArrayAdapter<Food>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jsonFileString = getJsonDataFromAsset(applicationContext,"Food_data.json")
+
+        Log.i("data",jsonFileString)
+
+        val gson = Gson()
+        val listItemType = object : TypeToken<ArrayList<Food>>(){}.type
+
+        var foodList : ArrayList<Food> = gson.fromJson(jsonFileString,listItemType)
+
+        itemList = foodList
+
+        val adapter = FoodAdapter(this@MainActivity,itemList)
+
+        listView.adapter = adapter
+
+
     }
 }
